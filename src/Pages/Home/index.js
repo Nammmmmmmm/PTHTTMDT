abcf429b5cda18a2c41ab3591b13b85dd7b2dae3
nakiet <cybell888@gmail.com>
2025-03-28 14:06:47 +0700

    v10

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,6 @@
 import HomeBanner from "../../Components/HomeBanner";
 import banner1 from "../../assets/images/banner1.jpg";
+import banner2 from "../../assets/images/banner2.jpg";
 import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
@@ -9,23 +10,38 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 
 import ProductItem from "../../Components/ProductItem/ProductItem";
+import HomeCat from "../../Components/HomeCat";
+
+import banner3 from "../../assets/images/banner3.jpg";
+import banner4 from "../../assets/images/banner4.jpg";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
+      <HomeCat />
+
+      
+
 
       <section className="homeProducts">
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-md-3">
-              <div className="banner">
-                <img src={banner1} className="cursor w-100" />
+              <div className="sticky">
+                <div className="banner">
+                  <img src={banner1} className="cursor w-100" />
+                </div>
+
+                <div className="banner mt-4">
+                  <img src={banner2} className="cursor w-100" />
+                </div>
               </div>
             </div>
 
             <div className="col-md-9 productRow">
               <div className="d-flex align-items-center">
+                
                 <div className="info w-75">
                   <h3 className="mb-0 hd">BEST SELLERS</h3>
                   <p className="text-light text-sml mb-0">
@@ -41,10 +57,9 @@ const Home = () => {
               <div className="product_row w-100 mt-4">
                 <Swiper
                   slidesPerView={4}
-                  spaceBetween={30}
-                  pagination={{
-                    clickable: true,
-                  }}
+                  spaceBetween={8}
+                  navigation={true}
+                  slidesPerGroup={3}
                   modules={[Navigation]}
                   className="mySwiper"
                 >
@@ -87,41 +102,28 @@ const Home = () => {
                 </Button>
               </div>
 
-              <div className="product_row w-100 mt-4">
-                <Swiper
-                  slidesPerView={4}
-                  spaceBetween={30}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  modules={[Navigation]}
-                  className="mySwiper"
-                >
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-
-                  <SwiperSlide>
-                    <ProductItem />
-                  </SwiperSlide>
-                </Swiper>
+              <div className="product_row productRow2 w-100 mt-4 d-flex">
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
               </div>
+
+              <div className="d-flex mt-4 mb-5 bannerSec">
+                <div className="banner">
+                  <img src={banner3} className="cursor w-100" />
+                </div>
+
+                <div className="banner">
+                  <img src={banner4} className="cursor w-100" />
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
