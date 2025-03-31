0d36f1288bc0a6f7634347b2bd2f0bd01cef74c3
nakiet <cybell888@gmail.com>
2025-03-31 11:32:05 +0700

    v16

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -7,7 +7,7 @@ import { useContext, useState } from "react";
 import { MyContext } from '../../App';
 
 
-const ProductItem = () => {
+const ProductItem = (props) => {
 
   const context = useContext(MyContext)
 
@@ -19,7 +19,7 @@ const ProductItem = () => {
 
   return (
     <>
-      <div className="item productItem">
+      <div className={`productItem ${props.itemView}`}>
       <div className="imgWrapper">
         <img src={pro1} className="w-100" />
 
